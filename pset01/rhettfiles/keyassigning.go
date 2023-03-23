c9df4738a51959cad6c64351d015f1a896a5893e
RLA <rhettapples@gmail.com>
2023-03-22 22:03:40 -0400

    working in the keyassigning.go file, working on hashing each block to create part of the pubkey

--- pset01/rhettfiles/keyassigning.go
+++ pset01/rhettfiles/keyassigning.go
@@ -4,9 +4,10 @@ package main
 import (
 	"fmt"
 	"crypto/rand"
+	"crypto/sha256"
 )
 
-type Block [2]byte
+type Block [32]byte
 
 type SecretKey struct {
 	ZeroPre [2]Block
@@ -21,20 +22,34 @@ type PublicKey struct {
 
 func main() {
 
+	z := []int{4,2,1,3}
+	fmt.Println(z)
+
 	var sk SecretKey
+	var pk PublicKey
 
 	// generate secret key ZeroPre
 
 	for i := 0; i < len(sk.ZeroPre); i++ {
+
+	
 		for j := 0; j < len(sk.ZeroPre[0]); j++ {
 			randbyte := make([]byte, 1)
 			_, err := rand.Read(randbyte)
 			if err != nil {
 				fmt.Println("error")
 			}
-			sk.ZeroPre[i][j] = randbyte[0]	
+			sk.ZeroPre[i][j] = randbyte[0] 
 		}
+
+		// hash each block 
+
+		hash := sk.ZeroPre[i].Hash()
+		pk.ZeroHash[i] = hash
+		
 	}
+
+	fmt.Printf("%x\n", pk.ZeroHash)
 
 	// Generate secretkey for OnePre
 
@@ -56,6 +71,6 @@ func main() {
 	
 }
 
-// func (self Block) Hash() Block {
-	// return sha256.Sum256(self[:])
-// }
+func (self Block) Hash() Block {
+	return sha256.Sum256(self[:])
+}
